46e34be1457969e53b8f5a7255eb5da7e2f4f85a
momo4creative <momo4creative@gmail.com>
2021-12-02 13:13:40 +0700

    perbaiki navbar

--- src/component/main/Navbar.jsx
+++ src/component/main/Navbar.jsx
@@ -4,10 +4,26 @@ import { Link, NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 w-2/12">
-      <NavLink>
-        <header>Main Menu</header>
-        <Link to="/todos">Todos</Link>
-      </NavLink>
+      <section>
+        <header className="text-gray-500">Main Menu</header>
+        <LinkTo label="Beranda" to="/" />
+        <LinkTo label="Todos" to="/todos" />
+        <LinkTo label="Login" to="/auth/login" />
+      </section>
     </nav>
   );
 }
+
+const LinkTo = ({ label, ...props }) => {
+  return (
+    <NavLink
+      {...props}
+      //   key={label}
+      className={`block px-4 py-2  hover:text-gray-100 hover:bg-gray-700 ${({
+        active,
+      }) => (active ? "text-blue-500" : "text-gray-400")}`}
+    >
+      {label}
+    </NavLink>
+  );
+};
